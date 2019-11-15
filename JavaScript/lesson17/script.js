569896e33085a4feea472dadac5f8c5af8223f2a
vitalii1608 <vitaliilazor@gmail.com>
2019-11-16 00:11:04 +0200

    дз js

---
+++ JavaScript/lesson17/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+
+let sec;
+
+let min;
+
+let hours;
+
+min = 60;
+
+hours = 60;
+
+sec = 3 *( min * min );
+
+console.log( sec );
+
+
+var num = 1;
+    num+= 12;
+    num-=  14;
+    num*= 5;
+    num/= 7;
+    num++;
+    num--;    
+
+alert(num);
+
+
+let a, b, c;
+
+a = 10;
+b = 2;
+c = 5;
+
+console.log( a + b + c );
+
+
+
+let d;
+
+let result;
+
+a = 17;
+b = 10;
+d = 7;
+
+c = a - b;
+
+result = c + d;
+
+console.log( result );
+
+
+let name;
+
+name = "Виталий";
+
+console.log( "Привет " + name );
---
+++ JavaScript/lesson18/script.js
@@ -0,0 +1,91 @@
+'use strict';
+
+
+
+//1
+
+let a;
+
+
+if( a === '1' ){
+
+    console.log( "Верно" );
+    
+} else{
+
+    console.log( "Неверно" );  
+}
+
+
+
+//2
+
+let item = false;
+
+if( item != true ){
+
+    console.log( "Верно" );
+    
+} else{
+
+    console.log( "Неверно" );  
+}
+
+
+//краткая форма
+
+item = false != true  ? true : false;
+
+console.log( item );
+
+
+
+
+//3
+
+
+if(  a > 0 && a < 4){
+
+    console.log( "Верно" );
+} else{
+
+    console.log( "Неверно" );  
+}
+
+
+//4
+
+let b;
+
+if( a > 3 && a < 12 || b >= 7 && b < 15 ){
+
+    console.log( "Верно" );
+} else{
+
+    console.log( "Неверно" );  
+}
+
+
+
+//5
+
+let month;
+
+month = 12;
+
+if( month > 0 && month < 3 || month == 12 ) {
+
+    console.log( "Зима" );
+} else if(month > 2 && month < 6){
+
+    console.log( "Весна" );  
+} else if(month > 5 && month < 9){
+
+    console.log( "Лето" );  
+} else if(month > 8 && month < 12){
+
+    console.log( "Осень" );  
+}else{
+
+    console.log( "Неверно" ); 
+}
